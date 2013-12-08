5d0313e635e17ca807923bd2955589039f5b459e
dtell <daniel.tell4@gmail.com>
2013-12-08 18:53:30 +0100

    prettier imp. of getref and getc + hlint + lookup

--- src/Papers.hs
+++ src/Papers.hs
@@ -76,29 +76,50 @@ data PaperGraph = PaperGraph {
 emptyGraph :: PaperGraph
 emptyGraph = buildPaperGraph [] M.empty 
 
-buildPaperGraph l m = PaperGraph (g l) m
+buildPaperGraph l = PaperGraph (g l)
 
 --allVerticesMapped :: [Integer] -> Map Integer Paper -> Bool
 --allVerticesMapped vs m = all (\k -> member k m) vs
 
 allVerticesMapped :: PaperGraph -> Bool
-allVerticesMapped (PaperGraph g m) = all (\k -> M.member k m) (vertices g)
+allVerticesMapped (PaperGraph g m) = all (`M.member` m) (vertices g)
 
 maxl :: (Ord a, Num a) => [(a,a)] -> a 
-maxl l | length l == 0 = 0
-       | otherwise     = maximum $ map maxt l
+maxl l | null l    = 0
+       | otherwise = maximum $ map maxt l
             where maxt (a,b) = max a b
     
-v = [(i,j) | i <- [0..3], j <- [0..3], i /= j]
+v = [(i,j) | i <- [0..3], j <- [0..3], i > j]
 gr = buildPaperGraph v (M.fromList [(i, Paper [Author "Pelle"] "Linjär algebra" "The Journal" 2013 ) | i <- [0..3]])
 
 fi :: (Integer, Integer) -> (Int, Int)    
 fi (a,b) = (fromIntegral a, fromIntegral b)
 
-doesCite (PaperGraph g _ ) x y = (x,y) `elem` (edges g)
+lookupPaper :: PaperGraph -> Vertex -> Paper
+lookupPaper (PaperGraph _ m) v = m M.! v 
 
---getCitations :: PaperGraph -> Int -> [Int]
---getCitations = 
+--getPapersBy :: PaperGraph -> Author -> [Vertex]
+--getPapersBy (PaperGraph g m) a =  [(v, lookupPaper v)  | v <- vertices g, lookupPaper]       
+
+--filter (\p -> a `elem` authors p) zip vs (map lookupPaper vs)
+  --  where vs = vertices g
+
+-- Does paper x cite paper x?
+doesCite :: PaperGraph -> Vertex -> Vertex -> Bool
+doesCite (PaperGraph g _ ) x y = (x,y) `elem` edges g
+
+-- Get all the papers that cites this paper
+getCitations :: PaperGraph -> Int -> [Vertex]
+getCitations = getl fst snd
+
+-- Get all the papers that this paper cites
+getReferences :: PaperGraph -> Int -> [Vertex]
+getReferences = getl snd fst
+
+type TupleElem = ((Vertex,Vertex) -> Vertex)
+
+getl :: TupleElem -> TupleElem -> PaperGraph -> Vertex -> [Vertex]
+getl f f' (PaperGraph g m) v = map f (filter (\e -> f' e == v) (edges g))
 
 --g :: [Edge] -> Graph
 g l = buildG (0, maxl l') l'
