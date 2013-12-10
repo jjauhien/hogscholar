e597f961d875ce5582a5d0163c5bfb13a3e5ec7f
Yauhen Yakimenka <jjauhien@gmail.com>
2013-12-10 11:15:06 +0100

    Just one more commit. /Y.

--- src/Crawler.hs
+++ src/Crawler.hs
@@ -11,6 +11,7 @@
 -- |
 --
 -----------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
 
 module Crawler where
 
@@ -21,8 +22,16 @@ import Network.CGI.Protocol
 import Network.HTTP
 import Network.URI
 
+-- import Network.HTTP.Conduit (simpleHttp)
+import qualified Data.Text as T
+import Text.HTML.DOM (parseLBS)
+import Text.XML.Cursor (Cursor, attributeIs, content, element, fromDocument, child,
+                        ($//), (&|), (&//), (>=>))
 
-import Papers
+import Data.ByteString.Lazy.Char8 (pack)
+
+
+import Papers (Author(Author), Paper, PaperGraph)
 
 data Query = Query {
       qAuthors :: [Author]
@@ -32,8 +41,36 @@ data Query = Query {
     }
 
 -- downloads paper and the papers which refer it (not more than n)
-downloadPaper :: Query -> Int -> IO (Maybe (Paper,[Paper]))
-downloadPaper q n = undefined
+downloadPaper :: Query -> IO (Maybe (Paper,[Paper]))
+downloadPaper q = undefined
+
+
+--downloadPaper' :: Query -> IO (Maybe (Paper, String))
+
+
+-- parses all the papers on the results page
+-- returns a list (Paper, Request to get list of citators)
+-- parsePage :: String -> [(Paper, Request String)]
+parsePage = map node2paper . ($// findPaperNodes) . fromDocument . parseLBS . pack
+
+
+-- all div elements, which have class "gs_r"
+findPaperNodes :: Cursor -> [Cursor]
+findPaperNodes = element "div" >=> attributeIs "class" "gs_r"
+
+
+-- parses one node which represents one paper
+--node2paper :: Cursor -> (Paper, Request String)
+node2paper cur = caption
+    where
+    caption =  {-T.concat . content . -}head .
+        ($// (element "h3" >=> attributeIs "class" "gs_rt")) $ cur
+
+findCaption = (element "h3" >=> attributeIs "class" "gs_rt")
+
+-- converts tag tree to simple text
+pruneTags node = undefined
+
 
 downloadHTML :: Request String -> IO (Maybe String)
 downloadHTML link = do
@@ -42,7 +79,7 @@ downloadHTML link = do
         Left _  -> return Nothing   -- some error
         Right r -> case rspCode r of
                     (2,_,_) -> return $ Just (rspBody r)
-                    _       -> return Nothing
+                    _       -> return Nothing  -- e.g. redirect FIXME
 
 
 -- creates query string
@@ -80,7 +117,9 @@ formRequest (Query authors title journal year) =
 formKV :: String -> Maybe String -> Maybe (String,String)
 formKV key val = (\v -> (key,v)) `fmap` val
 
+onlyK :: String -> Maybe (String, String)
 onlyK key = formKV key (Just "")
+
 
 dummyRequest = formRequest $
     Query [Author "Liu", Author "Lin", Author "Lin", Author "Chung"]
@@ -88,6 +127,7 @@ dummyRequest = formRequest $
         (Just "Digest of Technical Papers")
         (Just 2005)
 
+dummyPage = downloadHTML dummyRequest
 
 crawlPapers :: Query -> IO (Maybe PaperGraph)
 crawlPapers = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -19,11 +19,13 @@ module Main (
 
 import Crawler
 import Papers
+import PrettyPrinter
 
 
 main = undefined
 
 -- TODO: processing of command line
--- maybe use: System.Console.GetOpt (see http://www.haskell.org/haskellwiki/High-level_option_handling_with_GetOpt)
-parseArgs :: () -> Query
+-- maybe use: System.Console.GetOpt
+-- (see http://www.haskell.org/haskellwiki/High-level_option_handling_with_GetOpt)
+parseArgs :: IO Query
 parseArgs = undefined
